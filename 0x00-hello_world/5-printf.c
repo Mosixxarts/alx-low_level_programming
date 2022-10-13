bd20ea8c42ad3a48fb0c8f2436cedacf6ddd0552
Mosixxarts <omosesolaoluwa@gmail.com>
2022-10-13 01:02:10 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always () (Success)
+ */
+int main(void)
+{
+	prints("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
