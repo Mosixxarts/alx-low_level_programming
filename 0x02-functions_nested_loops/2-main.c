2e37b819fdad4716fb58f2952c0b3a128aca10fb
Mosixxarts <omosesolaoluwa@gmail.com>
2022-10-18 14:37:31 -0700

    task 2

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,3 +1,12 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	    print_alphabet_x10();
+	        return (0);
+}
