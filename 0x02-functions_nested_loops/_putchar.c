808c217b10240dcb8d11dc4e63ffc214ced41b58
Mosixxarts <omosesolaoluwa@gmail.com>
2022-10-18 07:51:19 -0700

    task 0

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,12 +2,12 @@
 
 /**
 *_putchar - write the character c to stdout
-*The character to be printed
+*@c: The character to be printed
 *
 *Return: On success 1
 *on error, -1 is returned and errno is set appropriately
 */
-int_putchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
