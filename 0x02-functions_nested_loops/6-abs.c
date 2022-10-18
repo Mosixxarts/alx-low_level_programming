7237f076adb74897a97130862d27f0ba986b1a23
Mosixxarts <omosesolaoluwa@gmail.com>
2022-10-18 15:29:11 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * abs -> return absolute value of a number
+ * @n: parameter
+ * Return: Absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
