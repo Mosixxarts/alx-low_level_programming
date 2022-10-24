f3d882f29d935cb252eb10f58379090a0dfdd93a
Mosixxarts <omosesolaoluwa@gmail.com>
2022-10-24 15:49:03 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,17 @@
 
 void print_rev(char *s)
 {
-	int i, len;
+	int i;
 
-	len = strlen(s);
-	for (i = len - 1; i >= 0; i--)
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i = i - 1 ; i >= 0; i--)
+	{
 		_putchar(s[i]);
-
-	_putchar('\n');
+	}
+	_putchar('\n')
 }
