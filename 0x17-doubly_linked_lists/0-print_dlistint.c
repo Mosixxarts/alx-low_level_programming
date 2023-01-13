558f10398dcc3b261e12b2d2db27ea46ba4fef6f
Mosixxarts <omosesolaoluwa@gmail.com>
2023-01-12 20:16:00 -0800

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,22 @@
-#!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-	counter = 0
-	for i in range(x):
-		try:
-		print("{}".format(my_list[i]), end="")
-		except IndexError:
-		break
-		else:
-		counter += 1
-		print()
-	return counter
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	size_t cont = 0;
+
+	while (node)
+	{
+		printf("%i\n", node->n);
+		cont++;
+		node = node->next;
+	}
+
+	return (cont);
+}
